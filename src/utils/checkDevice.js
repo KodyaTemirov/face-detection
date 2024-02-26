83952b76312bcf03a058a87c583158f22413c0c2
Xurshid <88909414+XurshidKarimov@users.noreply.github.com>
2024-02-26 19:18:36 +0500

    added checkDisplays

--- src/utils/checkDevice.js
+++ src/utils/checkDevice.js
@@ -30,3 +30,49 @@ export const checkMicrophone = async () => {
 		return false;
 	}
 };
+
+export const checkDisplays = async () => {
+	return await fetch('http://localhost:9060/')
+	.then(async response => {
+		const {body: readableStream} = response;
+
+		const reader = readableStream.getReader();
+
+		let data = [];
+
+		while(true){
+			const {done, value} = await reader.read();
+
+			if(done){
+				break;
+			}
+
+			data.push(value);
+		}
+
+		const totalLength = data.reduce((acc, chunk) => acc + chunk.length, 0);
+		const concatenedChunks = new Uint8Array(totalLength);
+		let offset = 0;
+
+		data.forEach(chunk => {
+			concatenedChunks.set(chunk, offset);
+			offset += chunk.length;
+		})
+
+		let concatedChunksToString = new TextDecoder().decode(concatenedChunks);
+		concatedChunksToString = JSON.stringify(concatedChunksToString);
+
+		const result = JSON.parse(concatedChunksToString);
+
+		const arrays = result.match(/\[[^\]]*\]/g);
+
+		const processes = arrays[0].replaceAll(/[\[, \], \"]/g, '').split(',');
+		const displays = arrays[1].replaceAll(/[\[, \], \"]/g, '').split(',');
+
+		return displays.length == 1;
+
+	})
+	.catch(error => {
+		return false;
+	});
+}

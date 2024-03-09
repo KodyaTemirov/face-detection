d5aa5b93e14d162702a6a4d387716f10ee9d377e
Xurshid <88909414+XurshidKarimov@users.noreply.github.com>
2024-03-09 12:47:11 +0500

    data replaced to data from gitlab
    data replaced to data from gitlab

---
+++ src/helpers/softUrls.js
@@ -0,0 +1,22 @@
+export default [
+	{
+		name: 'Proctoring RealSoft 1.0.0 Win32',
+		url: 'https://proctoring.platon.uz/services/platon-core/web/v1/store/file/apps/Proctoring_RealSoft-1.0.0_win32.exe',
+		icon: 'windows',
+	},
+	{
+		name: 'Proctoring RealSoft 1.0.0 Win64',
+		url: 'https://proctoring.platon.uz/services/platon-core/web/v1/store/file/apps/Proctoring_RealSoft-1.0.0_win64.exe',
+		icon: 'windows',
+	},
+	{
+		name: 'Proctoring RealSoft 1.0.0 Linux',
+		url: 'https://proctoring.platon.uz/services/platon-core/web/v1/store/file/apps/Proctoring_RealSoft-1.0.0.AppImage',
+		icon: 'linux',
+	},
+	{
+		name: 'Proctoring RealSoft 1.0.0 Macos',
+		url: 'https://proctoring.platon.uz/services/platon-core/web/v1/store/file/apps/Proctoring_RealSoft-1.0.0-arm64.dmg',
+		icon: 'macos',
+	},
+];

46ff1e794b4ca8899c3a6b84005b74029b186677
Xurshid <88909414+XurshidKarimov@users.noreply.github.com>
2024-03-09 12:47:51 +0500

    data replaced to data from gitlab
    data replaced to data from gitlab

---
+++ src/locales/index.js
@@ -0,0 +1,13 @@
+import en from './en.json';
+import ru from './ru.json';
+import la from './la.json';
+import uz from './uz.json';
+
+export const defaultLocale = 'la';
+
+export const languages = {
+	en,
+	ru,
+	la,
+	uz,
+};

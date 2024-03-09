b3515638346c5eac7fb3eb43e2e6508473d8916c
Xurshid <88909414+XurshidKarimov@users.noreply.github.com>
2024-03-09 12:35:15 +0500

    data replaced to data from gitlab
    data replaced to data from gitlab

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
+import svgLoader from 'vite-svg-loader';
 // https://vitejs.dev/config/
 export default defineConfig({
 	// TODO: возможно чуть позже понадобится.
@@ -25,5 +25,5 @@ export default defineConfig({
 			'@utils': '/src/utils/',
 		},
 	},
-	plugins: [vue()],
+	plugins: [vue(), svgLoader()],
 });
